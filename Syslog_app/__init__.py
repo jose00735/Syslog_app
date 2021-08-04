0cbb3f4068d340d8b9670e4cdd0b40ae2e2fadd5
Jose <josealejandroa99@gmail.com>
2021-08-04 14:55:44 +0000

    funciona

--- Syslog_app/__init__.py
+++ Syslog_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+#from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2f24ab45ab5abbed3857b751b903bbc7'
